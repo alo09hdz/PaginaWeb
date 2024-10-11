e209bd118f3948ed4dfb97cdd77ec7044430663f
TheFrisk777 <23610019@utgz.edu.mx>
2024-10-11 13:58:04 -0600

    Project Laracrud Punto Inicial

--- routes/web.php
+++ routes/web.php
@@ -17,32 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');  // Ruta de Bienvenida
 });
-
-Route::get('/testView', function () {
-    return view('testView');
-});
-
-Route::get('/texto', function () {
-    echo "Hi world";
-    echo "<h1> Hola Mundo </h1>";
-});
-
-Route::get('/echo', function () {
-    echo view('test2');
-});
-
-Route::get('/Masrutas', function () {
-    return "Podemos Tener Mas Rutas tipo Get,Post,Delete,Patch";
-});
-
-Route::get('/custom', function () {
-    $msj = "Mensaje desde el Servidor";
-    $id=" 500 ";
-    return view('custom', ['msj' => $msj, "identi" => $id, "A" => 29]);
-});
-
-Route::get('/contact', function () {
-    return view('contacto');
-}) -> name('contact');
-
-Route::get('/testdb', [TestController::class, 'test']);
